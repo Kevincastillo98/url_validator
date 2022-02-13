2dd99c4f72e4e00591b88d9a768282cdad71c90d
Kevin Castillo Roa <32142163+Kevincastillo98@users.noreply.github.com>
2022-02-12 21:07:22 -0600

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -1,6 +1,9 @@
-cat tesla2 | while read line; do
-request=$(curl -o /dev/null -I --silent -k --write-out "%{http_code}\n" $line)
-if [ $request == 200 ];
-then echo $line >> validos
-fi; 
+#!/bin/bash
+
+while read line;
+do
+	request=$(curl -o /dev/null -I --silent -k --write-out "%{http_code}\n" $line)
+	if [ $request == 200 ];
+		then echo $line >> validos
+	fi;
 done
